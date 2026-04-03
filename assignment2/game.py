717c92dcf263bacca16399214a30b31006052bd8
mackerel07 <blueknights170@gmail.com>
2026-04-03 11:12:59 +0900

    Add assignment2 files

---
+++ assignment2/game.py
@@ -0,0 +1,27 @@
+ class QuizGame:
+    def show_menu(self):
+        print("1.퀴즈 풀기")
+        print("2.퀴즈 추가")
+        print("3.퀴즈 목록")
+        print("4.점수 확인")
+        print("5.종료")
+
+    def run(self):
+        while True:
+            self.show_menu()   
+
+            choice = input("선택: ").strip() # 입력값 공백제거
+
+            if choice == "1":
+                print("퀴즈 풀기")
+            elif choice == "2":
+                print("퀴즈 추가")
+            elif choice == "3":
+                print("퀴즈 목록")
+            elif choice == "4":
+                print("점수 확인")
+            elif choice == "5":
+                print("종료합니다.")
+                break
+            else:
+                print("잘못된 입력입니다.")
