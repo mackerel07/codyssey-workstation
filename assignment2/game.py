9742d99f5fc6c723d3a268d7ba3dcdbcb71d7099
mackerel07 <blueknights170@gmail.com>
2026-04-06 22:51:30 +0900

    implement Quiz class and add basic play_quiz logic in QuizGame

--- assignment2/game.py
+++ assignment2/game.py
@@ -1,3 +1,5 @@
+from quiz import Quiz #quiz.py와 연동
+
 class QuizGame:
     def show_menu(self):
         print("1.퀴즈 풀기")
@@ -26,4 +28,41 @@ class QuizGame:
             else:
                 print("잘못된 입력입니다.")
 
-   
+    def __init__(self): #QuizGame의 생성자(매갸변수 필요x) **
+        self.quizzes = []
+        self.best_score = 0
+
+        #__init__(self,question, choices, answer)
+        q1 = Quiz(
+            "파이썬 만든 사람은?",
+            ["Guido", "Linus", "James", "Mark"], 
+            1
+        )
+
+        q2 = Quiz(
+            "리눅스 만든 사람은?",
+            ["Guido", "Linus", "James", "Mark"],
+            2
+        )
+
+        self.quizzes.append(q1)
+        self.quizzes.append(q2)
+
+    def play_quiz(self):
+        if not self.quizzes: #빈 리스트는 False(if not False)
+            print("퀴즈가 없습니다.")
+            return
+
+        score = 0
+
+        for q in self.quizzes: # q = q1, q2, q3
+            q.show()
+            answer = int(input("정답: "))
+
+            if q.check_answer(answer):
+                print("정답!")
+                score += 1
+            else:
+                print("오답!")
+
+        print(f"총 점수: {score}/{len(self.quizzes)}")
---
+++ assignment2/quiz.py
@@ -0,0 +1,13 @@
+class Quiz:
+    def __init__(self, question, choices, answer): # 위 변수들은 외부 값을 받기 위한 지역변수(init에서만 존재)
+        self.question = question
+        self.choices = choices #self.choices는 전체 리스트임
+        self.answer = answer
+
+    def show(self):
+        print("\n" + self.question)
+        for i, choice in enumerate(self.choices, 1): #인덱스의 1번부터 시작하도록 지정
+            print(f"{i}. {choice}") #enumerate로 생성한 인덱스번호+선지 출력, self.choices중 하나인 choice변수
+
+    def check_answer(self, user_input): #game.py에 True or False 반환e
+        return user_input == self.answer
